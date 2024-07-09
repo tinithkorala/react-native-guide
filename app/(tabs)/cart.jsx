2c94d27cae5ab6b09fde7f7ce12d466e0987f14e
tinithk <tinitse@gmail.com>
2024-07-10 03:18:46 +0530

    cart view developed

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -1,14 +1,95 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSelector } from "react-redux";
+
+// Icons
+import { Feather } from "@expo/vector-icons";
 
 const Cart = () => {
+  const cart = useSelector((state) => state.cart);
+
+  const { items, totalItems, totalPrice } = cart;
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        style={styles.listContainer}
+        data={items}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.cartRow}>
+            <Image
+              source={{ uri: item.image }}
+              style={styles.itemImage}
+              resizeMode="contain"
+            />
+            <View style={styles.itemDescription}>
+              <Text style={styles.itemName}>{item.name}</Text>
+              <View style={styles.cartControl}>
+                <Feather name="plus-square" size={24} color="black" />
+                <Text style={styles.itemQuantity}>{item.quantity}</Text>
+                <Feather name="minus-square" size={24} color="black" />
+              </View>
+              <Text style={styles.itemPrice}>{item.currency} {item.price}</Text>
+            </View>
+            <View style={styles.itemTotalPrice}>
+              <Text style={styles.itemPrice}>{item.currency} {item.price * item.quantity}</Text>
+            </View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Cart
+export default Cart;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+  listContainer: {
+    marginTop: 10,
+  },
+  cartRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  itemImage: {
+    width: 80,
+    height: 80,
+  },
+  itemDescription: {
+    flex: 1,
+    marginHorizontal: 10,
+  },
+  itemName: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  cartControl: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+  },
+  itemQuantity: {
+    marginHorizontal: 10,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  itemPrice: {
+    marginTop: 5,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  itemTotalPrice: {
+    alignItems: "flex-end",
+  },
+});
